cb7a3743e0a4f4ccb2825e513eeb056a8ea65bb4
mark <mark@titan.dom>
2018-10-03 21:53:25 +0200

    add scp urls for ip addresses

--- scpurl.sh
+++ scpurl.sh
@@ -2,3 +2,6 @@
 
 echo "$USER@`hostname --fqdn`:`pwd`"
 
+ip a | grep -w inet | grep -v 127.0.0.1 | awk '{print $2;}' | awk -F/ '{print $1;}' | while read IPADDR; do
+    echo "$USER@$IPADDR:`pwd`"
+done
