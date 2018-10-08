2c2e3f534bb2e5cd2dadaab8e295ab6b9d641741
Marcin Krol <mrkafk@gmail.com>
2018-10-08 21:28:38 +0200

    fix

--- borg_backup.sh
+++ borg_backup.sh
@@ -52,7 +52,7 @@ borg create                         \
     --exclude '/home/*/.cache/*'    \
     --exclude '/var/cache/*'        \
     --exclude '/var/tmp/*'          \
-    $(echo "$EXCLUDE" | while read x; do echo "--exclude $x   \\"; done)
+    $(echo "$EXCLUDE" | tr -s ' ' '\n' | while read x; do if [ -n "$x" ]; then echo "--exclude $x   \\"; fi; done)
     ::'{hostname}-{now}'            \
     $(echo "$BACKUP_DIRS" | tr -s ' ' '\n')
 
