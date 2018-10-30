d72cc0dfed05c220f5b11565110f966e5861e80f
Marcin Krol <mrkafk@gmail.com>
2018-10-30 12:30:04 +0100

    custom_bash_functions.sh

--- custom_bash_functions.sh
+++ custom_bash_functions.sh
@@ -103,3 +103,22 @@ function sbp () {
 	echo "Sourcing ~/.bash_profile in current shell."
 	source ~/.bash_profile
 }
+
+# Remove duplicates in PATH
+function remove_PATH_dups () {
+	if [ -n "$PATH" ]; then
+	old_PATH=$PATH:; PATH=
+	while [ -n "$old_PATH" ]; do
+		x=${old_PATH%%:*}       # the first remaining entry
+		case $PATH: in
+		*:"$x":*) ;;          # already there
+		*) PATH=$PATH:$x;;    # not there yet
+		esac
+		old_PATH=${old_PATH#*:}
+	done
+	PATH=${PATH#:}
+	unset old_PATH x
+	export PATH
+	fi
+}
+
