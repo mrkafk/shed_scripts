b57764085eb1027df1246c4c310c40e95ef4a7a0
Marcin Krol <mrkafk@gmail.com>
2018-11-03 12:39:33 +0100

    var_update_in_file.sh

---
+++ snppets.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+testn () {
+	if [ -z "$1" ]; then
+		echo "Param $2 empty. Aborting"
+		echo "Usage: $0 NAME MAC TYPE"
+		exit 1
+	fi
+}
+
---
+++ var_update_in_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+. "$SCRIPTDIR"/snippets.sh
+
+FILENAME="$1"
+VARNAME="$2"
+VALUE="$3"
+
+testn "$FILENAME" FILENAME
+testn "$VARNAME" VARNAME
+testn "$VALUE" VALUE
+
