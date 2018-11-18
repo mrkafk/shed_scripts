53f79b96e9b5e03f576394dba3dcc941e5c585dc
mrkafk <mrkafk@gmail.com>
2018-11-18 21:50:34 +0100

    Update files: add_jk_user_export2.sh

--- add_jk_user_export2.sh
+++ add_jk_user_export2.sh
@@ -7,23 +7,39 @@ if [ -z "$NEWUSER" ]; then
 	exit 1
 fi
 
-mkdir -p /export2/"$NEWUSER"
-chown root:root /export2/"$NEWUSER"
+BASE=$BASE
 
-jk_init -v -f -j /export2/"$NEWUSER" basicshell editors extendedshell netutils ssh scp rsync sftp
+mkdir -p $BASE/"$NEWUSER"
+chown root:root $BASE/"$NEWUSER"
+
+jk_init -v -f -j $BASE/"$NEWUSER" basicshell editors extendedshell netutils ssh scp rsync sftp
 
 groupadd "$NEWUSER"
-mkdir -p /export2/"$NEWUSER"/home/"$NEWUSER"
-useradd -d "/export2/$NEWUSER/./home/$NEWUSER" -s /usr/sbin/jk_chrootsh -g "$NEWUSER" "$NEWUSER"
-chown "$NEWUSER":"$NEWUSER" /export2/"$NEWUSER"/home/"$NEWUSER"
-mkdir /export2/"$NEWUSER"/tmp
-chmod 777 /export2/"$NEWUSER"/tmp
+mkdir -p $BASE/"$NEWUSER"/home/"$NEWUSER"
+useradd -d "$BASE/$NEWUSER/./home/$NEWUSER" -s /usr/sbin/jk_chrootsh -g "$NEWUSER" "$NEWUSER"
+chown "$NEWUSER":"$NEWUSER" $BASE/"$NEWUSER"/home/"$NEWUSER"
+mkdir $BASE/"$NEWUSER"/tmp
+chmod 777 $BASE/"$NEWUSER"/tmp
 
-jk_cp -v -k -f  /export2/"$NEWUSER"/ /usr/bin/ssh-keygen
+jk_cp -v -k -f  $BASE/"$NEWUSER"/ /usr/bin/ssh-keygen
+chmod 755 $BASE/"$NEWUSER"/bin
 
-cat /etc/passwd | grep -w "$NEWUSER" | head -n 1 >> /export2/"$NEWUSER"/etc/passwd
-perl -pi -e "s/\/export2\/$NEWUSER\/\.//g" /export2/"$NEWUSER"/etc/passwd
-perl -pi -e "s/usr\/sbin\/jk_chrootsh/bin\/bash/g" /export2/"$NEWUSER"/etc/passwd
+cat /etc/passwd | grep -w "$NEWUSER" | head -n 1 >> $BASE/"$NEWUSER"/etc/passwd
+perl -pi -e "s/\$BASE\/$NEWUSER\/\.//g" $BASE/"$NEWUSER"/etc/passwd
+perl -pi -e "s/usr\/sbin\/jk_chrootsh/bin\/bash/g" $BASE/"$NEWUSER"/etc/passwd
 
-cat /etc/group | grep -w "$NEWUSER" | head -n 1 >> /export2/"$NEWUSER"/etc/group
+cat /etc/group | grep -w "$NEWUSER" | head -n 1 >> $BASE/"$NEWUSER"/etc/group
 
+PW=$(pwgen -A 11 | head -n 1 | awk '{print $1;}')
+echo -e "$PW\n$PW" | passwd "$NEWUSER"
+
+echo "Password for $NEWUSER: $PW"
+
+SSHDIR="$BASE/$NEWUSER/home/$NEWUSER/.ssh"
+mkdir -p "$SSHDIR"
+chown "$NEWUSER:$NEWUSER" "$SSHDIR"
+chmod 700 "$SSHDIR"
+echo "Paste public SSH key, finish with Ctrl-D"
+set -x
+cat >>"$SSHDIR"/authorized_keys
+set +x
