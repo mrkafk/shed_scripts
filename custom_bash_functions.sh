e0762f6f9cdd9dead62d20c099d83b877ebefc5a
mrkafk <mrkafk@gmail.com>
2018-11-30 11:49:30 +0100

    hf display function definition

--- custom_bash_functions.sh
+++ custom_bash_functions.sh
@@ -20,6 +20,10 @@ function va () {
 	TMP1=/tmp/$$.1
 	TMP2=/tmp/$$.2
 	find . -name activate | grep -w 'bin/activate' | awk '{print NR " " $0 ;}' > "$TMP1"
+	if [ -s "$TMP1" ]; then
+		echo "No Python virtual environments found."
+		return
+	fi
 	whiptail --clear --menu "Select virtualenv:" 24 70 $(wc -l "$TMP1" | cut -d " " -f 1)  $(cat "$TMP1")  2>"$TMP2"
 	# dialog --clear --menu "Select virtualenv:" 24 70 $(wc -l "$TMP1" | cut -d " " -f 1)  $(cat "$TMP1")  2>"$TMP2"
 	NUM=$(cat $TMP2)
