4f9a3fd0de426774961a77e65db0ac40bea65823
mrkafk <mrkafk@gmail.com>
2019-01-03 21:50:15 +0100

    Update files: fail2ban-unban-all.sh

--- fail2ban-unban-all.sh
+++ fail2ban-unban-all.sh
@@ -4,8 +4,10 @@ JAILS=`fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | 
 for JAIL in $JAILS
 do
   fail2ban-client status "$JAIL" | grep 'Banned IP list:' | awk -F"\t" '{print $2;}' | tr -s ' ' | tr ' ' '\n' | while read IP; do
-    set -x
-    fail2ban-client set "$JAIL" unbanip "$IP"
-    set +x
+    if [ -n "$IP" ]; then
+      set -x
+      fail2ban-client set "$JAIL" unbanip "$IP"
+      set +x
+    fi
   done
 done
