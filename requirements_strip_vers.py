ae367b7f510c5fa2f5d2a1bf08ce373977d0eb72
mrkafk <mrkafk@gmail.com>
2020-02-20 21:37:57 +0100

    Update files: requirements_strip_vers.py

--- requirements_strip_vers.py
+++ requirements_strip_vers.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
-def find_requirements_file():
+def find_requirements_file(fname='requirements.txt'):
+    fname = 'requirements.txt'
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
     try:
-        fo = open('requirements.txt', 'rb')
-    except OSError:
-        print >>sys.stderr, 'File requirements.txt not found'
+        fo = open(fname, 'r')
+    except OSError as exc:
+        print(exc)
         sys.exit(1)
     return fo
 
 def strip_vers(fo):
     lines = [line.split('=') for line in fo]
-    return filter(None, [line[0] for line in lines])
+    return [_f for _f in [line[0] for line in lines] if _f]
 
 def write_reqs_nover():
     fo = find_requirements_file()
     vers = strip_vers(fo)
     outfname = 'requirements_nover.txt'
-    with open(outfname, 'wb') as fo:
+    with open(outfname, 'w') as fo:
         fo.write('\n'.join(vers))
-    print >>sys.stderr, 'Wrote output to', outfname
+    print('Wrote output to', outfname, file=sys.stderr)
 
 if __name__ == '__main__':
     write_reqs_nover()
