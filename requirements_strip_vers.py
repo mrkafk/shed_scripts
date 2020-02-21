a389622392d4648415eeef8ee62ea5a4ab22f372
mrkafk <mrkafk@gmail.com>
2020-02-21 14:16:43 +0100

    Update files: requirements_strip_vers.py

--- requirements_strip_vers.py
+++ requirements_strip_vers.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2020 Marcin Krol <mrkafk@gmail.com>.  All rights reserved.
+
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appear in all copies and that
+# both that copyright notice and this permission notice appear in
+# supporting documentation.
+
+# MARCIN KROL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS,
+# IN NO EVENT SHALL MARCIN KROL BE LIABLE FOR ANY SPECIAL, INDIRECT
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# This software is licensed under MIT License:
+# http://opensource.org/licenses/mit-license.php
+
+
 import sys
 
 def find_requirements_file(fname='requirements.txt'):
